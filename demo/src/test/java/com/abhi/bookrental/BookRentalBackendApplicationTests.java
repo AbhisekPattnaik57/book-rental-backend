2515e3e1cd07889e1d50f6414dc20137cc2256f0
AbhisekPattnaik57 <abhisekpattnaik298@gmail.com>
2025-05-16 18:10:04 +0530

    Initial commit - backend setup with Spring Boot

---
+++ demo/src/test/java/com/abhi/bookrental/BookRentalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhi.bookrental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRentalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
